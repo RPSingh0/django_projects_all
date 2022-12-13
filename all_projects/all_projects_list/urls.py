b38dce959c31bfcc37a38c66314b6c0c9a5cc567
RPSingh0 <rpalsingh715@gmail.com>
2022-12-13 20:00:00 +0530

    started adding forms

--- all_projects/all_projects_list/urls.py
+++ all_projects/all_projects_list/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view=views.ListAllProjects.as_view())
+    path("", view=views.ListAllProjects.as_view(), name="list-all-projects"),
+    path("add_project", view=views.AddNewProject.as_view(),
+         name="add-new-project")
 ]
--- all_projects/all_projects_list/views.py
+++ all_projects/all_projects_list/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView
+from django.views.generic.edit import FormView
 from . import models
 
 
@@ -10,3 +12,13 @@ class ListAllProjects(ListView):
     model = models.ProjectList
     template_name = "all_projects_list/index.html"
     context_object_name = "project_list"
+
+
+class AddNewProject(FormView):
+    """
+    This class creates a from based on the model: `models.ProjectList`
+    """
+
+    form_class = forms.AddNewProject
+    template_name = "all_projects_list/add_new_project.html"
+#     success_url = reverse("list-all-projects")
