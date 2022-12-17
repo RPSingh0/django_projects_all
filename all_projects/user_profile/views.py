eaf7411d8532cfadd0807f7c1b7fd7619570886e
RPSingh0 <rpalsingh715@gmail.com>
2022-12-17 18:04:41 +0530

    added user's all project list

--- all_projects/user_profile/views.py
+++ all_projects/user_profile/views.py
@@ -6,3 +6,9 @@ class ShowUser(DetailView):
     template_name = "user_profile/index.html"
     model = models.UserProfile
     context_object_name = "user"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+
+    #     # get all the projects made by the user
+    #     models.UserProfile
